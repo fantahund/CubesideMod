7e6767a6d75e096a4ddc5424ae8f94e0c68c5317
fanta <tobiasmuellner@outlook.com>
2021-01-03 02:36:42 +0100

    Add Change Narrator Key

--- src/main/java/de/fanta/cubeside/CubesideClient.java
+++ src/main/java/de/fanta/cubeside/CubesideClient.java
@@ -3,8 +3,12 @@ package de.fanta.cubeside;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class CubesideClient implements ClientModInitializer {
@@ -12,4 +16,11 @@ public class CubesideClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
     }
+
+    public static final KeyBinding NARRATOR_KEYBINDING = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "options.narrator",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_B,
+            "key.categories.misc"
+    ));
 }
---
+++ src/main/java/de/fanta/cubeside/mixin/MixinKeyboard.java
@@ -0,0 +1,18 @@
+package de.fanta.cubeside.mixin;
+
+
+import de.fanta.cubeside.CubesideClient;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.Keyboard;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(Keyboard.class)
+public class MixinKeyboard {
+
+    @ModifyConstant(method = "onKey",  constant = @Constant(intValue = 66))
+    private int narratorkey(int old) {
+        return KeyBindingHelper.getBoundKeyOf(CubesideClient.NARRATOR_KEYBINDING).getCode();
+    }
+}
