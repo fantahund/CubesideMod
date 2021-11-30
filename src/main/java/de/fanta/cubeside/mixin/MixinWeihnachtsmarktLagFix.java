13e7a4221fdd90b209219cac16a3a16860497768
fanta <tobiasmuellner@outlook.com>
2021-11-30 10:53:34 +0100

    Fix WeihnachtsmarktLeg Fix :>

--- src/main/java/de/fanta/cubeside/mixin/MixinWeihnachtsmarktLagFix.java
+++ src/main/java/de/fanta/cubeside/mixin/MixinWeihnachtsmarktLagFix.java
@@ -2,6 +2,8 @@ package de.fanta.cubeside.mixin;
 
 import de.fanta.cubeside.Config;
 import net.minecraft.client.render.Frustum;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.decoration.ArmorStandEntity;
@@ -10,24 +12,25 @@ import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(net.minecraft.client.render.entity.EntityRenderer.class)
 public class MixinWeihnachtsmarktLagFix<T extends Entity> {
 
-    @Inject(at = @At("HEAD"), method = "shouldRender")
-    public void render(T entity, Frustum frustum, double x, double y, double z, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(at = @At("HEAD"), method = "render", cancellable = true)
+    public void render(T entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
         if (!Config.weihnachtsmarkt) {
             return;
         }
         if (entity instanceof ItemFrameEntity itemFrame) {
             if (itemFrame.getHeldItemStack().getItem() == Items.FILLED_MAP) {
-                cir.setReturnValue(false);
+                ci.cancel();
             }
         }
         if (entity instanceof ArmorStandEntity armorStand) {
             if (armorStand.getEquippedStack(EquipmentSlot.HEAD).getItem() != Items.PLAYER_HEAD && armorStand.isInvisible()) {
-                cir.setReturnValue(false);
+                ci.cancel();
             }
         }
     }
