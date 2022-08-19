1668742d0b9eca3ebf36ac25df5c306d5b2aeebb
fanta <tobiasmuellner@outlook.com>
2022-08-19 04:50:49 +0200

    rebind Debug Screen (F3)

--- src/main/java/de/fanta/cubeside/KeyBinds.java
+++ src/main/java/de/fanta/cubeside/KeyBinds.java
@@ -7,6 +7,7 @@ import org.lwjgl.glfw.GLFW;
 
 public class KeyBinds {
     public static KeyBinding NARRATOR_KEYBINDING;
+    public static KeyBinding F3_KEYBINDING;
     public static KeyBinding AUTO_CHAT;
     public static KeyBinding TOGGLE_GAMMA;
     public static KeyBinding TOGGLE_SHOW_ENTITIES_IN_SPECTATOR_MODE;
@@ -16,6 +17,13 @@ public class KeyBinds {
                 "options.narrator",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_B,
+                "key.categories.misc"
+        ));
+
+        F3_KEYBINDING = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "options.debugscreen",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_F3,
                 "key.categories.misc"
         ));
 
